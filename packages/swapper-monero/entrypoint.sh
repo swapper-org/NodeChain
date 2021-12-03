333a69c7862d0b7f048d603d5769c3b962923127
swapper-phoenix <phoenix@swapper.market>
2021-12-03 13:10:27 +0100

    Finish dockerfile for monero with entrypoint

--- packages/swapper-monero/entrypoint.sh
+++ packages/swapper-monero/entrypoint.sh
@@ -1,26 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-if [ $(echo "$1" | cut -c1) = "-" ]; then
-  echo "$0: assuming arguments for bitcoind"
-
-  set -- bitcoind "$@"
+# if thrown flags immediately,
+# assume they want to run the blockchain daemon
+if [ "${1:0:1}" = '-' ]; then
+	set -- monerod "$@"
 fi
 
-if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
-  mkdir -p "$BITCOIN_DATA"
-  chmod 700 "$BITCOIN_DATA"
-  chown -R bitcoin "$BITCOIN_DATA"
-
-  echo "$0: setting data directory to $BITCOIN_DATA"
-
-  set -- "$@" -datadir="$BITCOIN_DATA"
+# if they are running the blockchain daemon,
+# make efficient use of memory
+if [ "$1" = 'monerod' ]; then
+	numa='numactl --interleave=all'
+	if $numa true &> /dev/null; then
+		set -- $numa "$@"
+	fi
+	exec "$@"
 fi
 
-if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
-  echo
-  exec gosu bitcoin "$@"
-fi
-
-echo
+# otherwise, don't get in their way
 exec "$@"
