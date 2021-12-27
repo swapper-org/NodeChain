d6f19931926b4214c409b16d3617a1ee146df0f6
swapper-phoenix <phoenix@swapper.market>
2021-12-27 09:43:24 +0100

    Improve xmr packages and create default wallet

--- packages/nodechain-monero-wallet/entrypoint.sh
+++ packages/nodechain-monero-wallet/entrypoint.sh
@@ -7,17 +7,15 @@ if [ $(echo "$1" | cut -c1) = "-" ]; then
   set -- monero-wallet-rpc "$@"
 fi
 
-
 if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "monero-wallet-rpc" ]; then
   mkdir -p "$MONERO_DATA"
+  mv "./wallet-dir" "$MONERO_DATA/wallet-dir"
   chmod 700 "$MONERO_DATA"
   chown -R monero "$MONERO_DATA"
-  mv swapper "$MONERO_DATA/swapper"
-  mv swapper.keys "$MONERO_DATA/swapper.keys"
 
   echo "$0: setting data directory to $MONERO_DATA"
 
-  set -- "$@" --log-file="$MONERO_DATA/debug.log" --wallet-file="$MONERO_DATA/swapper" --password="swapper"
+  set -- "$@" --wallet-dir="$MONERO_DATA/wallet-dir" --log-file="$MONERO_DATA/debug.log" # --config-file="$MONERO_DATA/monero-wallet-cli.conf"
 fi
 
 echo
