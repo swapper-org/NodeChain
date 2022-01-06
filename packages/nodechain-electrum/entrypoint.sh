0bc7adb2d184f8d138e9de871ed53e95a67b34ff
swapper-phoenix <phoenix@swapper.market>
2022-01-06 15:27:42 +0100

    First attempt to replace electrumx to electrs

--- packages/nodechain-electrum/entrypoint.sh
+++ packages/nodechain-electrum/entrypoint.sh
@@ -9,6 +9,6 @@ Electrum/run_electrum --offline setconfig rpcuser "swapper" $electrum_args
 Electrum/run_electrum --offline setconfig rpcpassword "swapper" $electrum_args
 Electrum/run_electrum --offline setconfig rpcport "30000" $electrum_args
 Electrum/run_electrum --offline setconfig oneserver "true" $electrum_args
-Electrum/run_electrum --offline setconfig server "electrumx:50001:t" $electrum_args
+Electrum/run_electrum --offline setconfig server "electrs:50001:t" $electrum_args
 
 Electrum/run_electrum daemon $electrum_args
