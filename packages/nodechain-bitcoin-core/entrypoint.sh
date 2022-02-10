990070c156bc1f2ae941dbdb64b90584cc979a69
swapper-phoenix <phoenix@swapper.market>
2022-02-10 18:26:17 +0100

    Update bitcoin core with default wallet creation

--- packages/nodechain-bitcoin-core/entrypoint.sh
+++ packages/nodechain-bitcoin-core/entrypoint.sh
@@ -19,6 +19,21 @@ fi
 
 if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
   echo
+  if [ "${NETWORK}" = "regtest" ]; then
+    WALLETDIR="$BITCOIN_DATA/regtest"
+  elif [ "${NETWORK}" = "testnet" ]; then
+    WALLETDIR="$BITCOIN_DATA/testnet3"
+  elif [ "${NETWORK}" = "mainnet" ]; then
+    WALLETDIR="$BITCOIN_DATA/mainnet"
+  fi
+  
+  WALLETFILE="${WALLETDIR}/wallet.dat"
+  mkdir -p "${WALLETDIR}"
+  chown -R bitcoin:bitcoin "${WALLETDIR}"
+  if ! [ -f "${WALLETFILE}" ]; then
+    echo "The wallet does not exists, creating it at ${WALLETDIR}..."
+    gosu bitcoin bitcoin-wallet "-wallet=" create
+  fi
   exec gosu bitcoin "$@"
 fi
 
