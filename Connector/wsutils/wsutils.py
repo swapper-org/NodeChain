e371ddc21a49ed7fc0d37f73635b40963ed8d5c8
swapper-pegasus <pegasus@swapper.market>
2022-02-28 23:30:39 +0100

    ETH tests refactor

--- Connector/wsutils/wsutils.py
+++ Connector/wsutils/wsutils.py
@@ -1,28 +1,5 @@
 #!/usr/bin/python3
-from logger import logger
 from .constants import WS_METHOD
-
-
-webSockets = []
-webSocketMethods = {}
-webSocketClosingHandlers = []
-
-
-def webSocket(f):
-    logger.printInfo(f"Registering new websocket: {f.__name__}")
-    webSockets.append(f)
-    return f
-
-
-def webSocketMethod(f):
-    logger.printInfo(f"Registering new websocket method: {f.__name__}")
-    webSocketMethods[f.__name__] = f
-    return f
-
-
-def webSocketClosingHandler(f):
-    logger.printInfo(f"Registering new websocket handler: {f.__name__}")
-    webSocketClosingHandlers.append(f)
 
 
 def isWsEnpointPath(method):
