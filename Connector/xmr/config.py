6a888ffc723f9d489dc1cb6ef254cd39f0d99b86
swapper-phoenix <phoenix@swapper.market>
2022-03-30 21:00:04 +0200

    Update package to fit the new configuration

--- Connector/xmr/config.py
+++ Connector/xmr/config.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 from json import JSONEncoder
-from utils import utils
 
 
 class Config:
@@ -9,24 +8,11 @@ class Config:
 
         self._coin = coin
         self._networkName = networkName
-        self._protocol = ""
-        self._host = ""
-        self._rpcPort = 0
-        self._rpcPath = ""
 
-    def __attachNetworkToHost(self, host):
-        return f"{host}-{self.networkName}"
+        self._monerodRpcEndpoint = ""
 
     def loadConfig(self, config):
-
-        defaultConfig, err = utils.loadDefaultPackageConf(self.coin)
-        if err is not None:
-            return False, err
-
-        self.protocol = config["protocol"] if "protocol" in config else defaultConfig["protocol"]
-        self.host = config["host"] if "host" in config else self.__attachNetworkToHost(defaultConfig["host"])
-        self.rpcPort = config["rpcPort"] if "rpcPort" in config else defaultConfig["rpcPort"]
-        self.rpcPath = config["rpcPath"] if "rpcPath" in config else defaultConfig["rpcPath"]
+        self.monerodRpcEndpoint = ["monerodRpcEndpoint"]
 
         return True, None
 
@@ -43,40 +29,12 @@ class Config:
         self._networkName = value
 
     @property
-    def protocol(self):
-        return self._protocol
+    def monerodRpcEndpoint(self):
+        return self._monerodRpcEndpoint
 
-    @protocol.setter
-    def protocol(self, value):
-        self._protocol = value
-
-    @property
-    def host(self):
-        return self._host
-
-    @host.setter
-    def host(self, value):
-        self._host = value
-
-    @property
-    def rpcPort(self):
-        return self._rpcPort
-
-    @rpcPort.setter
-    def rpcPort(self, value):
-        self._rpcPort = value
-
-    @property
-    def rpcPath(self):
-        return self._rpcPath
-
-    @rpcPath.setter
-    def rpcPath(self, value):
-        self._rpcPath = value
-
-    @property
-    def rpcEndpoint(self):
-        return f"{self.protocol}://{self.host}:{self.rpcPort}/{self.rpcPath}"
+    @monerodRpcEndpoint.setter
+    def monerodRpcEndpoint(self, value):
+        self._monerodRpcEndpoint = value
 
     def jsonEncode(self):
         return ConfigEncoder().encode(self)
@@ -86,8 +44,5 @@ class ConfigEncoder(JSONEncoder):
 
     def encode(self, o):
         return {
-            "protocol": o.protocol,
-            "host": o.host,
-            "rpcPort": o.rpcPort,
-            "rpcPath": o.rpcPath
+            "monerodRpcEndpoint": o.monerodRpcEndpoint
         }
