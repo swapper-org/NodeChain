baee1396a77537d4eefc727b04f61c276b5a8ec5
swapper-pegasus <pegasus@swapper.market>
2022-04-12 14:32:32 +0200

    ERC 20 getAddressBalance

---
+++ Connector/eth/erc20/constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+ERC20_STANDARD_SYMBOL = "erc20"
+RPC_JSON_SCHEMA_FOLDER = "eth/erc20/rpcschemas/"
+ABI_FOLDER = "eth/erc20/abi/"
+SCHEMA_CHAR_SEPARATOR = "_"
+REQUEST = "request"
+RESPONSE = "response"
+SCHEMA_EXTENSION = ".json"
+
+BALANCE_OF_METHOD_ECR20_ABI = "balanceOf"
+
+GET_ADDRESS_BALANCE = "getaddressbalance"
+GET_ADDRESSES_BALANCE = "getaddressesbalance"
---
+++ Connector/eth/erc20/utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import json
+from .constants import *
+from logger import logger
+from httputils import error
+
+
+def getMethodSchemas(name):
+    return getRequestMethodSchema(name), getResponseMethodSchema(name)
+
+
+def getRequestMethodSchema(name):
+    return RPC_JSON_SCHEMA_FOLDER + name + SCHEMA_CHAR_SEPARATOR + REQUEST + SCHEMA_EXTENSION
+
+
+def getResponseMethodSchema(name):
+    return RPC_JSON_SCHEMA_FOLDER + name + SCHEMA_CHAR_SEPARATOR + RESPONSE + SCHEMA_EXTENSION
+
+
+def getABIFilename(name):
+    return f"{ABI_FOLDER}{name}{SCHEMA_EXTENSION}"
+
+
+def getFunctionABI(fileName):
+
+    try:
+        with open(fileName) as file:
+            return json.load(file)
+    except FileNotFoundError as err:
+        logger.printError(f"Schema {fileName} not found: {err}")
+        raise error.InternalServerError(f"Schema {fileName} not found: {err}")
